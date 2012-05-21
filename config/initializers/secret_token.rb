b79d48389cbde0346d57e117b5c7a6b5bacaddb8
weyewe <w.yunnal@gmail.com>
2012-05-21 15:35:17 +0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Salorg::Application.config.secret_token = 'db5f155f11d8f4035448a1969d86bca9af151be701e44d76e6c92ad9128c0cb8eeea9e6785c7837f16f01f5e84c952d7995e174393ada610879591788ff79ecb'
